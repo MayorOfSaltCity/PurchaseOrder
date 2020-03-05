db786bf4b153c702d98993cbab4512476e119c5a
Paul Harrington <info@paulharrington.co.za>
2020-03-06 01:36:20 +0200

    added controllers

---
+++ PurchaseOrder/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PurchaseOrder.Models;
+
+
+namespace PurchaseOrder.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            var rng = new Random();
+            return null;
+        }
+    }
+}
---
+++ PurchaseOrder/Controllers/SupplierController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PurchaseOrder.Models;
+
+
+namespace PurchaseOrder.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SupplierController : ControllerBase
+    {
+
+        private readonly ILogger<SupplierController> _logger;
+
+        public SupplierController(ILogger<SupplierController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Supplier> Get()
+        {
+            var rng = new Random();
+            return null;
+        }
+    }
+}
