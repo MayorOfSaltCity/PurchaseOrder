775d518b84159d3047f954cefe2d28504ee09ac7
Paul Harrington <info@paulharrington.co.za>
2020-03-07 17:06:30 +0200

    added shell for PO tests

---
+++ PurchaseOrderUnitTests/PurchaseOrderControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PurchaseOrderUnitTests
+{
+    [TestClass]
+    public class PurchaseOrderControllerTests
+    {
+        [TestMethod]
+        public void TestCreatePurchaseOrder()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestAddItemToPurchaseOrder()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestRemoveItemFromPurchaseOrder()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestListPurchaseOrders()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestGetPurchaseOrder()
+        {
+            Assert.Fail();
+        }
+    }
+}
